40b91a1e2432b119b1ff18561668a9f346fe52f1
KYUNG MO KWEON <kkweon@gmail.com>
2017-04-03 15:08:06 -0700

    feat: gym uploader helper

--- gym_uploader.py
+++ gym_uploader.py
@@ -2,11 +2,13 @@ import argparse
 import configparser
 import gym
 
+
 def read_config(file='gym.ini'):
     parser = configparser.ConfigParser()
     parser.read(file)
 
     return parser
+
 
 def read_argparse():
     parser = argparse.ArgumentParser()
@@ -18,16 +20,16 @@ def read_argparse():
 def upload(gym_path, key):
     return gym.upload(gym_path, api_key=key)
 
+
 if __name__ == '__main__':
     config = read_config()
     args = read_argparse()
     key = config['default']['GYM_API_KEY']
 
-    if len(key) == 0 or None:
+    if len(key) == 0 or key is None or key == "YOUR_API_KEY":
         print("Please enter the API key in gym.ini ")
-    
+
     else:
         result = upload(args.path, key)
         if not result:
             print("error")
-
